a4d7afcb0e2eb19fe5351f5237650e94c57c0502
corto <corto@example.com>
2019-01-12 12:42:20 +0100

    Added practical examples of Stack data structure

---
+++ basic_data_structures/stacks/balanced_parenthesis.py
@@ -0,0 +1,23 @@
+from Stack import Stack
+
+def parenthesis_checker(symbol_string):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    return balanced and s.is_empty()
+
+
+print(parenthesis_checker("((())))"))
+print(parenthesis_checker("((()))"))
