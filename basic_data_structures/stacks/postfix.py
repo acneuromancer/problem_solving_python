dc8b19715d93db0783a389c96096adcefea495cb
corto <corto@example.com>
2019-01-13 19:20:18 +0100

    Added postfix evaluation example

--- basic_data_structures/stacks/postfix.py
+++ basic_data_structures/stacks/postfix.py
@@ -33,4 +33,34 @@ def infix_to_postfix(infix_expr):
     return " ".join(postfix_list)
 
 
-print(infix_to_postfix("( A + B ) * ( C + D )"))
+def postfix_eval(postfix_expr):
+    operand_stack = Stack()
+    token_list = postfix_expr.split()
+
+    for token in token_list:
+        if token in "0123456789":
+            operand_stack.push(int(token))
+        else:
+            operand2 = operand_stack.pop()
+            operand1 = operand_stack.pop()
+            result = do_math(token, operand1, operand2)
+            operand_stack.push(result)
+
+    return operand_stack.pop()
+
+def do_math(op, op1, op2):
+    if op == '*':
+        return op1 * op2
+    elif op == '/':
+        return op1 / op2
+    elif op == '+':
+        return op1 + op2
+    else:
+        return op1 - op2
+
+
+print(infix_to_postfix("A * B + C * D"))
+print(infix_to_postfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+
+result = postfix_eval('7 8 + 3 2 + /')
+print(result)
