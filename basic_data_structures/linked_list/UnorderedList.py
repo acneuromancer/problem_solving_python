754c6354aca1df091aeed128ee1a20a76412db63
corto <corto@example.com>
2019-01-16 00:01:44 +0100

    Added UnorderedList.

---
+++ basic_data_structures/linked_list/UnorderedList.py
@@ -0,0 +1,24 @@
+from Node import Node
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+
+        while current != None:
+            count += 1
+            current = current.get_next()
+
+        return count
---
+++ basic_data_structures/linked_list/ul_test.py
@@ -0,0 +1,8 @@
+from UnorderedList import UnorderedList
+
+l = UnorderedList()
+
+for i in range(10):
+    l.add(i)
+    
+print(l.size())
