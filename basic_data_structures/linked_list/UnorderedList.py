d4a49a09df05ba5fe8aaaea9c36d89231685ed67
corto <corto@example.com>
2019-01-16 20:03:05 +0100

    Added append and remove methods to UnorderedList class.

--- basic_data_structures/linked_list/UnorderedList.py
+++ basic_data_structures/linked_list/UnorderedList.py
@@ -33,3 +33,38 @@ class UnorderedList:
             current = current.get_next()
 
         return found
+
+    def append(self, value):
+        temp = Node(value)
+
+        if self.head == None:
+            self.head = temp
+            return
+
+        current = self.head
+        while current.get_next() != None:
+            current = current.get_next()
+
+        current.set_next(temp)
+
+    def remove(self, value):
+        current = self.head
+        previous = None
+        found = False
+
+        while not found and current != None:
+            if current.get_data() == value:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if not found:
+            return False
+
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+        return True
--- basic_data_structures/linked_list/ul_test.py
+++ basic_data_structures/linked_list/ul_test.py
@@ -1,9 +1,34 @@
 from UnorderedList import UnorderedList
 
+def print_list(ul):
+    node = ul.head
+
+    while node != None:
+        print(node.get_data(), end=" ")
+        node = node.get_next()
+
+    print()
+
+
 l = UnorderedList()
 
 for i in range(10):
-    l.add(i)
+    l.append(i)
 
-print(l.search(-10))
-print(l.search(5))
+print_list(l)
+
+l.add(-1000)
+l.append(-3000)
+print_list(l)
+
+# l.remove(-10)
+l.remove(-1000)
+print_list(l)
+
+l.remove(-3000)
+print_list(l)
+
+print(l.remove(5))
+print_list(l)
+
+print(l.remove(-101))
