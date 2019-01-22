8d584b117a17b8d340119eae35a0d242ad91eb41
corto <dokukodo108@gmail.com>
2019-01-22 01:11:17 +0100

    Added shell sort function.

---
+++ sorting/shell.py
@@ -0,0 +1,21 @@
+def shell_sort(numbers):
+    n = len(numbers)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range (gap, n):
+            temp = numbers[i]
+            j = i
+
+            while j >= gap and numbers[j-gap] > temp:
+                numbers[j] = numbers[j-gap]
+                j -= gap
+
+            numbers[j] = temp
+
+        gap //= 2
+
+
+numbers = [3000, 0, -1, 25, 10, 8]
+shell_sort(numbers)
+print(numbers)
