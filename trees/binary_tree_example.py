36a954d161799f04990da464f1cf5dfeca9e51bf
corto <dokukodo108@gmail.com>
2019-01-23 22:10:14 +0100

    Added binary tree preparative examples.

---
+++ trees/binary_tree_example.py
@@ -0,0 +1,6 @@
+my_tree = ['a', [ 'b', ['d', [],[]], ['e', [], []] ], ['c', ['f', [], []], [] ] ]
+
+print(my_tree)
+print('root: ', my_tree[0])
+print('Left subtree = ', my_tree[1])
+print('Right subtree = ', my_tree[2])
