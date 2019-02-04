1c059b22a2f48c543f45a70dc8febc17dc17803a
corto <dokukodo108@gmail.com>
2019-02-04 22:07:56 +0100

    Some reorganizing of the file structure.

--- trees/binary_tree/bin_tree_parser.py
+++ trees/binary_tree/bin_tree_parser.py
@@ -33,6 +33,20 @@ class BinaryTree:
     def get_root_val(self):
         return self.key
 
+    def preorder(self):
+        print(self.key)
+        if self.left_child:
+            self.left_child.preorder()
+        if self.right_child:
+            self.right_child.preorder()
+
+    def postorder(self):
+        if self.left_child:
+            self.left_child.postorder()
+        if self.right_child:
+            self.right_child.postorder()
+        print(self.key)
+
 
 class Stack:
 
@@ -55,6 +69,17 @@ class Stack:
         return len(self.items)
 
 
+'''
+1. If the current token is a ‘(’, add a new node as the left child of the current node, and
+descend to the left child.
+2. If the current token is in the list [‘+’,‘−’,‘/’,‘*’], set the root value of the current node to
+the operator represented by the current token. Add a new node as the right child of the
+current node and descend to the right child.
+3. If the current token is a number, set the root value of the current node to the number and
+return to the parent.
+4. If the current token is a ‘)’, go to the parent of the current node.
+'''
+
 def build_parse_tree(fp_exp):
     fp_list = fp_exp.split()
     p_stack = Stack()
@@ -63,3 +88,47 @@ def build_parse_tree(fp_exp):
     current_tree = e_tree
 
     for i in fp_list:
+        if i == '(':
+            current_tree.insert_left('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_left_child()
+        elif i not in ['+', '-', '*', '/', ')']:
+            current_tree.set_root_val(int(i))
+            parent = p_stack.pop()
+            current_tree = parent
+        elif i in ['+', '-', '*', '/']:
+            current_tree.set_root_val(i)
+            current_tree.insert_right('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_right_child()
+        elif i == ')':
+            current_tree = p_stack.pop()
+        else:
+            raise ValueError
+
+    return e_tree
+
+
+import operator
+
+def evaluate(parse_tree):
+    opers = {
+        '+' : operator.add,
+        '-' : operator.sub,
+        '*' : operator.mul,
+        '/' : operator.truediv
+    }
+
+    left = parse_tree.get_left_child()
+    right = parse_tree.get_right_child()
+
+    if left and right:
+        fn = opers[parse_tree.get_root_val()]
+        return fn(evaluate(left), evaluate(right))
+    else:
+        return parse_tree.get_root_val()
+
+
+pt = build_parse_tree("( ( 10 + 5 ) * 3 )")
+pt.postorder()
+print(evaluate(pt))
