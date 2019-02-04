50d19fec918a6ceebcf81e1fa71d2f93f82601df
corto <dokukodo108@gmail.com>
2019-02-04 19:13:23 +0100

    Added a binary tree parser.

---
+++ trees/parser.py
@@ -0,0 +1,116 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+class BinaryTree:
+
+    def __init__(self, root):
+        self.key = root
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left(self, new_node):
+        if self.left_child == None:
+            self.left_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.left_child = self.left_child
+            self.left_child = t
+
+    def insert_right(self, new_node):
+        if self.right_child == None:
+            self.right_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_child = self.right_child
+            self.right_child = t
+
+    def get_right_child(self):
+        return self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root_val(self, obj):
+        self.key = obj
+
+    def get_root_val(self):
+        return self.key
+
+    def preorder(self):
+        print(self.key)
+        if self.left_child:
+            self.left_child.preorder()
+        if self.right_child:
+            self.right_child.preorder()
+
+    def postorder(self):
+        if self.left_child:
+            self.left_child.postorder()
+        if self.right_child:
+            self.right_child.postorder()
+        print(self.key)
+
+
+'''
+1. If the current token is a ‘(’, add a new node as the left child of the current node, and
+descend to the left child.
+2. If the current token is in the list [‘+’,‘−’,‘/’,‘*’], set the root value of the current node to
+the operator represented by the current token. Add a new node as the right child of the
+current node and descend to the right child.
+3. If the current token is a number, set the root value of the current node to the number and
+return to the parent.
+4. If the current token is a ‘)’, go to the parent of the current node.
+'''
+
+def build_parse_tree(fp_exp):
+    fp_list = fp_exp.split()
+    p_stack = Stack()
+    e_tree = BinaryTree('')
+    p_stack.push(e_tree)
+    current_tree = e_tree
+
+    for i in fp_list:
+        if i == '(':
+            current_tree.insert_left('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_left_child()
+        elif i not in ['+', '-', '*', '/', ')']:
+            current_tree.set_root_val(int(i))
+            parent = p_stack.pop()
+            current_tree = parent
+        elif i in ['+', '-', '*', '/']:
+            current_tree.set_root_val(i)
+            current_tree.insert_right('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_right_child()
+        elif i == ')':
+            current_tree = p_stack.pop()
+        else:
+            raise ValueError
+
+    return e_tree
+
+
+pt = build_parse_tree("( ( 10 + 5 ) * 3 )")
+pt.postorder()
+print()
+pt = build_parse_tree("( 3 + ( 4 * 5 ) )")
+pt.preorder()
