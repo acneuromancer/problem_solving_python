bc07f72086ad31d42a91b19e32f4463fd032541e
corto <corto@example.com>
2019-02-17 19:56:21 +0100

    Added a file of four-letter world and word_letter.py as an example of practical use of graph data structure.

---
+++ graphs/word_ladder.py
@@ -0,0 +1,28 @@
+from Graph import Graph
+
+def buildGraph(wordFile):
+    d = {}
+    g = Graph()
+
+    with open(wordFile, 'r') as wfile:
+        for line in wfile:
+            for w in line.split():
+                for i in range(len(w)):
+                    bucket = w[:i] + '_' + w[i+1:]
+                    print(bucket)
+
+                    if bucket in d:
+                        d[bucket].append(w)
+                    else:
+                        d[bucket] = [w]
+
+        for bucket in d.keys():
+            for word1 in d[bucket]:
+                for word2 in d[bucket]:
+                    if word1 != word2:
+                        g.addEdge(word1, word2)
+
+    return g
+
+
+buildGraph('words.txt')
